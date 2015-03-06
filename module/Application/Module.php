d4e67f7dacb473a6b45ca92ae04e9c4830547ecc
Otávio Guilherme Arruda dos Santos <otavio.arruda@telecontrol.com.br>
2015-03-06 13:31:24 -0300

    M3Ex1 -> EventManager & SharedEventManager

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -19,8 +19,15 @@ class Module
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH, array($this, 'onDispatch'), 100);
     }
 
+    public function onDispatch(MvcEvent $e){
+        $viewModel = $e->getViewModel();
+        $viewModel->setVariable('categories', 'CATEGORY LIST');
+
+    }
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
